2f575345e4e04a4cd6d0a13fab485d50cf4bdd0d
Edson Funke <edsonfunke@gmail.com>
2016-05-12 23:05:05 -0300

    Ajustes de arquivos ignorados no repositorio

---
+++ aula01/AloMundo.java
@@ -0,0 +1,13 @@
+/* Isto é um comentário do programa
+ */
+ import java.lang.String; 
+public class AloMundo
+{
+ String nome = "Edson";
+
+  public static void main (String args[]) 
+  {
+
+    System.out.println("Boa noite "+ nome);    
+  }
+}
